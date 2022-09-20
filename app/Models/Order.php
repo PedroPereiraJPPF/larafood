5b0dbb0a970ea321113ff04a5033cec69ab21ec1
Pedro <joaopedropereira201@gmail.com>
2022-09-20 17:38:24 -0300

    Adicionada a tabela de orders

---
+++ app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Tenant\Traits\TenantTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use TenantTrait;
+
+    protected $fillable = ['tenant_id', 'identify', 'client_id', 'table_id', 'total', 'status', 'comment'];
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+
+
+
+}
